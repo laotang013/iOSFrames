6b7655d1e1ef3d750d16ca19a4bb92a1e88aa6e5
Start <250226337@qq.com>
2017-11-22 17:51:02 +0800

    ddd

--- DynamicDemo/DynamicDemo/UIAttachmentBehaviorController.m
+++ DynamicDemo/DynamicDemo/UIAttachmentBehaviorController.m
@@ -23,6 +23,8 @@
 @property(nonatomic,strong)UIDynamicAnimator *animator;
 /**吸附属性*/
 @property(nonatomic,strong)UIAttachmentBehavior *attachBehavior;
+/**UIPushBehavior*/
+@property(nonatomic,strong)UIPushBehavior *pushBehavior;
 @end
 
 @implementation UIAttachmentBehaviorController
@@ -51,6 +53,12 @@
     [self.view addSubview:self.squareView];
     [self.squareView addSubview:self.brownView];
     [self.view addSubview:self.redView];
+    [self test2];
+ 
+}
+//吸附
+-(void)test1
+{
     //设置平移
     UIPanGestureRecognizer *panGes = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panGesSelector:)];
     [self.view addGestureRecognizer:panGes];
@@ -65,13 +73,64 @@
     //self.attachBehavior = [[UIAttachmentBehavior alloc]initWithItem:self.squareView offsetFromCenter:offset attachedToAnchor:self.redView.center];
     [self.animator addBehavior:collision];
     self.attachBehavior  = [[UIAttachmentBehavior alloc]initWithItem:self.squareView attachedToAnchor:self.redView.center];
+    [self.attachBehavior setFrequency:1.0f];//设置对象的振幅和摆动大小
+    [self.attachBehavior setDamping:0.1f];//用于校平动画峰值
+    [self.attachBehavior setLength:100.f];//根据其初始位置进行调整
     [self.animator addBehavior:self.attachBehavior];
 }
-
 -(void)panGesSelector:(UIPanGestureRecognizer *)panGes
 {
     CGPoint tapPoint = [panGes locationInView:self.view];
     [self.attachBehavior setAnchorPoint:tapPoint];
     self.redView.center = tapPoint;
+    //瞬间位移
+//    UISnapBehavior *snapBehavior = [[UISnapBehavior alloc]initWithItem:self.redView snapToPoint:tapPoint];
+//    snapBehavior.damping = 0.75f;
+//    [self.animator addBehavior:snapBehavior];
+    //推力计算
+    CGPoint origin = CGPointMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds));
+    CGFloat distance = sqrtf(powf(tapPoint.x - origin.x,2.0)+powf(tapPoint.y-origin.y, 2.0));
+    CGFloat angle = atan2(tapPoint.y-origin.y, tapPoint.x-origin.x);
+    distance = MIN(distance, 100.f);
+    [self.pushBehavior setMagnitude:angle];
+    [self.pushBehavior setActive:YES];
 }
+
+
+//推力
+-(void)test2
+{
+    //设置物理属性
+    self.animator = [[UIDynamicAnimator alloc]initWithReferenceView:self.view];
+    UICollisionBehavior *collectionBehavior = [[UICollisionBehavior alloc]initWithItems:@[self.squareView]];
+    collectionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    UIPushBehavior *pushBehavior = [[UIPushBehavior alloc]initWithItems:@[self.squareView] mode:UIPushBehaviorModeInstantaneous];
+    self.pushBehavior = pushBehavior;
+    pushBehavior.angle = 0.0;
+    pushBehavior.magnitude = 0.0f;
+    [self.animator addBehavior:pushBehavior];
+    //设置平移
+    UIPanGestureRecognizer *panGes = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(panGesSelector:)];
+    [self.view addGestureRecognizer:panGes];
+    
+    //元素属性
+    
+}
+
+-(void)test3
+{
+    /*
+     * UIDynamic 可以为继承UIView的控件添加物理行为。
+     * Dynamic Animator动画者 为动力学元素提供物理学相关的能力及动画。同时为这些元素提供相关的上下文。
+     * 是动力学与底层iOS物理引擎之间的中介。将Behavior对象添加到Animator即可实现动力仿真。
+     * Dynamic Animator Item:动力学元素，是任何遵守了UIDynamic协议的对象，从iOS7开始，UIView和UICollectionViewLayoutAttributes默认实现协议，如果自定义对象实现了该协议，即可通过Dynamic Animator实现物理仿真。
+     * 具体实现步骤：
+     1、创建一个仿真者[UIDynamicAnimator] 用来仿真所有的物理行为
+     2、创建具体的物理仿真行为[如重力UIGravityBehavior]
+     3、将物理仿真行为添加给仿真者实现仿真效果。
+     
+
+     */
+}
+
 @end
